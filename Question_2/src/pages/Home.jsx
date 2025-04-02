0f2d5a5edd286b71bf3da5c9c7e94a941bcc57df
Aditya <adityaroy2169@gmail.com>
2025-04-02 21:26:51 +0530

    added screenshots

--- Question_2/src/pages/Home.jsx
+++ Question_2/src/pages/Home.jsx
@@ -3,27 +3,40 @@ import { Link } from "react-router";
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-6">
-      <h1 className="text-5xl font-bold mb-8 text-gray-800">Social Media Analytics</h1>
-      <nav className="bg-white p-6 rounded-lg shadow-md">
-        <ul className="flex flex-col space-y-4">
-          <li>
-            <Link to="/top-users" className=" text-lg font-medium px-4 py-2 rounded hover:bg-blue-50 block">
-              Top Users
-            </Link>
-          </li>
-          <li>
-            <Link to="/trending-posts" className=" text-lg font-medium px-4 py-2 rounded hover:bg-blue-50 block">
-              Trending Posts
-            </Link>
-          </li>
-          <li>
-            <Link to="/feed" className=" text-lg font-medium px-4 py-2 rounded hover:bg-blue-50 block">
-              Feed
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="min-h-screen bg-gray-100  justify-center p-6">
+      <h1 className="text-5xl font-bold mb-8 text-gray-800 text-center">
+        Social Media Analytics
+      </h1>
+      <div className="h-full flex flex-col items-center">
+        <nav className="bg-white p-6 rounded-lg shadow-md sm:h-[50vh] sm:w-[50vw] h-[80vh] w-[90vw]">
+          <ul className="flex flex-col space-y-6 items-center">
+            <li>
+              <Link
+                to="/top-users"
+                className=" text-2xl font-medium px-4 py-2 rounded hover:bg-blue-50 block"
+              >
+                Top Users
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/trending-posts"
+                className=" text-2xl font-medium px-4 py-2 rounded hover:bg-blue-50 block"
+              >
+                Trending Posts
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/feed"
+                className=" text-2xl font-medium px-4 py-2 rounded hover:bg-blue-50 block"
+              >
+                Feed
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
--- README.md
+++ README.md
@@ -17,3 +17,16 @@ npm i
 npm run dev -- --port 3000
 
 Frontend post is 3000
+
+## ScteenShots
+
+### Question 1
+
+![Screenhot](./Question_1/screenshots/Backend.png)
+![Screenhot](./Question_1/screenshots/Backend2.png)
+
+### Question 2
+
+![Screenhot](./Question_2/screenshots/ReactHome.png)
+![Screenhot](./Question_2/screenshots/TopUsers.png)
+![Screenhot](./Question_2/screenshots/TopUsersMobile.png)
